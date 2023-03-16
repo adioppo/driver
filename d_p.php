ddd37dab82840f5959149d317f6f82cfcfcf6117
adioppo <115898990+adioppo@users.noreply.github.com>
2023-03-16 14:54:12 +0200

    Add files via upload

---
+++ d_p.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Driver_portrait</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/driver_portrait.css">
+</head>
+<body>
+	<div id='container_all_div_id'>
+		<header id='header_id'>
+			<article id='logo_article_id'>
+				<div id='logo_div_id'>logo</div>
+			</article>
+			<article id='butoane_chat_article_id'>
+				<div id='buton_fb_header_id'>but fb</div>
+				<div id='buton_wh_header_id'>but wh</div>
+			</article>
+			<article id='meniu_navigare_article_id'>
+				<div id='buton_acasa_header_id'>acasa</div>
+				<div id='buton_galerie_foto_header_id'>foto</div>
+				<div id='buton_despre_header_id'>despre</div>
+			</article>
+		</header>
+		<div id='scroller'> <!-- divul asta permite scrolarea si, astfel, pot sa tin pe loc headerul, fara sticky-->		
+			<section id='section_id'>
+			section
+									Lorem ipsum dolor sit amet. Rem molestias voluptatem eos necessitatibus consequatur qui omnis pariatur est consectetur totam et atque voluptatem ab molestias laboriosam vel doloribus minus. Eum ipsam consequatur et molestiae placeat et optio aliquid aut similique voluptas aut mollitia sint et praesentium quia sit quidem expedita. Ex debitis tenetur sit velit recusandae hic officiis quod eum incidunt voluptatem in officiis eaque qui eveniet pariatur. Ut error ducimus aut necessitatibus quia aut fugiat eveniet sed voluptatum totam et modi facere et temporibus autem. Qui neque galisum aut molestiae illo qui tempora assumenda. Est tempora aliquid vel quia tenetur et sequi ducimus ut facere tenetur. Est ipsam nisi sed odit veritatis et sint atque qui eligendi voluptas et sequi magnam! Ut pariatur iure vel harum aliquam id distinctio facere ex cumque doloribus aut eaque nesciunt ut natus explicabo. Ut pariatur omnis est commodi enim id natus inventore est nostrum quia et expedita quaerat 33 itaque harum. Rem mollitia voluptate qui quia dolorum ut dolore asperiores et corporis accusantium ad iure magnam?			
+			Lorem ipsum dolor sit amet. Rem molestias voluptatem eos necessitatibus consequatur qui omnis pariatur est consectetur totam et atque voluptatem ab molestias laboriosam vel doloribus minus. Eum ipsam consequatur et molestiae placeat et optio aliquid aut similique voluptas aut mollitia sint et praesentium quia sit quidem expedita. Ex debitis tenetur sit velit recusandae hic officiis quod eum incidunt voluptatem in officiis eaque qui eveniet pariatur. Ut error ducimus aut necessitatibus quia aut fugiat eveniet sed voluptatum totam et modi facere et temporibus autem. Qui neque galisum aut molestiae illo qui tempora assumenda. Est tempora aliquid vel quia tenetur et sequi ducimus ut facere tenetur. Est ipsam nisi sed odit veritatis et sint atque qui eligendi voluptas et sequi magnam! Ut pariatur iure vel harum aliquam id distinctio facere ex cumque doloribus aut eaque nesciunt ut natus explicabo. Ut pariatur omnis est commodi enim id natus inventore est nostrum quia et expedita quaerat 33 itaque harum. Rem mollitia voluptate qui quia dolorum ut dolore asperiores et corporis accusantium ad iure magnam?			
+			Lorem ipsum dolor sit amet. Rem molestias voluptatem eos necessitatibus consequatur qui omnis pariatur est consectetur totam et atque voluptatem ab molestias laboriosam vel doloribus minus. Eum ipsam consequatur et molestiae placeat et optio aliquid aut similique voluptas aut mollitia sint et praesentium quia sit quidem expedita. Ex debitis tenetur sit velit recusandae hic officiis quod eum incidunt voluptatem in officiis eaque qui eveniet pariatur. Ut error ducimus aut necessitatibus quia aut fugiat eveniet sed voluptatum totam et modi facere et temporibus autem. Qui neque galisum aut molestiae illo qui tempora assumenda. Est tempora aliquid vel quia tenetur et sequi ducimus ut facere tenetur. Est ipsam nisi sed odit veritatis et sint atque qui eligendi voluptas et sequi magnam! Ut pariatur iure vel harum aliquam id distinctio facere ex cumque doloribus aut eaque nesciunt ut natus explicabo. Ut pariatur omnis est commodi enim id natus inventore est nostrum quia et expedita quaerat 33 itaque harum. Rem mollitia voluptate qui quia dolorum ut dolore asperiores et corporis accusantium ad iure magnam?			
+
+			Lorem ipsum dolor sit amet. Rem molestias voluptatem eos necessitatibus consequatur qui omnis pariatur est consectetur totam et atque voluptatem ab molestias laboriosam vel doloribus minus. Eum ipsam consequatur et molestiae placeat et optio aliquid aut similique voluptas aut mollitia sint et praesentium quia sit quidem expedita. Ex debitis tenetur sit velit recusandae hic officiis quod eum incidunt voluptatem in officiis eaque qui eveniet pariatur. Ut error ducimus aut necessitatibus quia aut fugiat eveniet sed voluptatum totam et modi facere et temporibus autem. Qui neque galisum aut molestiae illo qui tempora assumenda. Est tempora aliquid vel quia tenetur et sequi ducimus ut facere tenetur. Est ipsam nisi sed odit veritatis et sint atque qui eligendi voluptas et sequi magnam! Ut pariatur iure vel harum aliquam id distinctio facere ex cumque doloribus aut eaque nesciunt ut natus explicabo. Ut pariatur omnis est commodi enim id natus inventore est nostrum quia et expedita quaerat 33 itaque harum. Rem mollitia voluptate qui quia dolorum ut dolore asperiores et corporis accusantium ad iure magnam?			
+
+			<article id='prezentare_generala_article_id'>
+				</article>
+				<article id='galerie_foto_article_id'>
+				</article>
+				<article id='prezentare_detalii_article_id'>
+				</article>
+			</section>
+			</div>
+			<footer id='footer_id'>
+			footer
+				<article id='contact_article_id'></article>
+				<article id='copyright_article_id'></article>
+			</footer>
+		
+	</div>
+</body>
+</html>
